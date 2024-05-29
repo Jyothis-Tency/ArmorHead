9e46c8f6b54425f2a871c6050d479af760db5c2a
jyothis <jyothisgtency@gmail.com>
2024-05-29 19:28:05 +0530

    img size inc

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/admin", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   session({
