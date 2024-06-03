8cc44cede5c5ff748948f8bb6d51b98695856d6d
jyothis <jyothisgtency@gmail.com>
2024-06-03 17:05:25 +0530

    crypt

--- helper/passwordHelper.js
+++ helper/passwordHelper.js
@@ -1,9 +1,20 @@
 const bcrypt = require("bcryptjs");
+const argon2 = require('argon2')
+
+// const securePassword = async (password) => {
+//   try {
+//     console.log("securePassword triggered");
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     return hashedPassword;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
 
 const securePassword = async (password) => {
   try {
     console.log("securePassword triggered");
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await argon2.hash(password);
     return hashedPassword;
   } catch (error) {
     console.log(error.message);
